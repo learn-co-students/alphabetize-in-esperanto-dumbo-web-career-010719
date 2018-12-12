222dba29f289c0dd615db993637729f1f7ede0cd
mazenswar <mazenswar@protonmail.com>
2018-12-11 21:22:08 -0500

    alphatize

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,16 @@
+require "pry"
 def alphabetize(arr)
   # code here
+  esperantoABC = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  eArr = esperantoABC.split("")
+
+  arr.sort do |a,b|
+    i = 0
+    until esperantoABC.index(a[i]) != esperantoABC.index(b[i])
+      i+=1
+    end
+    esperantoABC.index(a[i]) <=> esperantoABC.index(b[i])
+  end
+
+  #binding.pry
 end
