be11c149dcf154977cdabb3636ef10c69e47ab15
Edwin Lin <edwinlin@gmail.com>
2018-12-16 08:49:52 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,14 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+p arr
+
+  arr2 = arr.sort_by do |ele| 
+    ele.each_char do |char|
+      ESPERANTO_ALPHABET.index(char)
+    end
+  end
+    
+  return arr2.sort_by{|ele| ESPERANTO_ALPHABET.index(ele[0])}
+
 end
